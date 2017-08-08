04de6407b66950b71aeddff534ad36bdf7ce46c8
Daisyrie Dumali <daisyrie.dumali@goabroad.com>
2017-08-08 13:28:32 +0800

    react 3

---
+++ js/apidata.js
@@ -0,0 +1,113 @@
+var apidata_directories = {
+    "directories": [
+        {
+            "id": 1,
+            "name": "US University",
+            "alias": "us-university",
+            "abbreviation": "USU"
+        },
+        {
+            "id": 2,
+            "name": "Volunteer Abroad",
+            "alias": "volunteer-abroad",
+            "abbreviation": "VA"
+        },
+        {
+            "id": 3,
+            "name": "Teach Abroad",
+            "alias": "teach-abroad",
+            "abbreviation": "TA"
+        },
+        {
+            "id": 4,
+            "name": "Intern Abroad",
+            "alias": "intern-abroad",
+            "abbreviation": "Intern"
+        },
+        {
+            "id": 5,
+            "name": "Jobs Abroad",
+            "alias": "jobs-abroad",
+            "abbreviation": "JA"
+        },
+        {
+            "id": 6,
+            "name": "Study Abroad",
+            "alias": "study-abroad",
+            "abbreviation": "SA"
+        },
+        {
+            "id": 7,
+            "name": "Language School",
+            "alias": "language-study-abroad",
+            "abbreviation": "LS"
+        },
+        {
+            "id": 8,
+            "name": "Adventure Travel",
+            "alias": "adventure-travel-abroad",
+            "abbreviation": "AT"
+        },
+        {
+            "id": 9,
+            "name": "High School",
+            "alias": "highschool-study-abroad",
+            "abbreviation": "HS"
+        },
+        {
+            "id": 10,
+            "name": "GoAbroad",
+            "alias": "goabroad",
+            "abbreviation": "GA"
+        },
+        {
+            "id": 11,
+            "name": "TEFL Certificate Course",
+            "alias": "tefl-courses",
+            "abbreviation": "TEFL"
+        },
+        {
+            "id": 12,
+            "name": "Degree Abroad",
+            "alias": "degree-abroad",
+            "abbreviation": "DA"
+        },
+        {
+            "id": 13,
+            "name": "Learning Abroad",
+            "alias": "learning-abroad",
+            "abbreviation": "LA"
+        },
+        {
+            "id": 15,
+            "name": "Scholarships",
+            "alias": "scholarships-abroad",
+            "abbreviation": "SCH"
+        },
+        {
+            "id": 17,
+            "name": "Accommodations",
+            "alias": "accommodations",
+            "abbreviation": "SCH"
+        },
+        {
+            "id": 18,
+            "name": "Embassies",
+            "alias": "embassies",
+            "abbreviation": "EMB"
+        }
+    ],
+    "meta": {
+        "links": [
+            {
+                "relation": "_self",
+                "href": "\/directories",
+                "method": "GET"
+            }
+        ],
+        "count": 16,
+        "offset": 0,
+        "limit": 20,
+        "sort": []
+    }
+}
