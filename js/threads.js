cdccd90b995557c93832b4eed11ea4273d5b1d6b
Daisyrie Dumali <daisyrie.dumali@goabroad.com>
2017-08-08 08:52:09 +0800

    init commit

---
+++ js/threads.js
@@ -0,0 +1,14 @@
+var threads = [{
+				rank: 1,
+				vote: 1,
+				title: 'FBI finally hacks iPhone, ending court battle with Apple',
+				description: "The FBI's sudden success in circumventing Apple's security measures might raise new questions about whether Apple's products are truly ironclad. It will also lead to heightened speculation over the \"outside party\" that assisted the government at the last minute and criticism from some quarters over the FBI's failure to hack into an iPhone on its own.",
+				submittedBy: 'mashable',
+				commentCount: 0,
+				comments: [
+					{
+						name: "Nate Ruess",
+						comment: "I'm nothing without love chos"
+					}
+				]
+			  }];
